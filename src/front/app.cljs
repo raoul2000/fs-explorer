39b454116a1725d020994ee3fa239386baae4b3d
raoul2000 <manu34@free.fr>
2024-01-27 16:29:20 -0300

    front: config test

--- src/front/app.cljs
+++ src/front/app.cljs
@@ -1,13 +1,14 @@
 (ns app)
 
 (defn run []
-  (js/console.log "boo "))
+  (js/console.log "boo !"))
 
 ;;  Lifecycle Hooks =================================
 ;; see https://shadow-cljs.github.io/docs/UsersGuide.html#_lifecycle_hooks
 
 (defn ^:dev/before-load stop []
   (js/console.log "/before-load"))
+
 
 (defn ^:dev/after-load start []
   (js/console.log "after-load")
