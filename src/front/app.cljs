bd3bcc7ac355f2b97ba3a40265f5a42e70e9509f
raoul2000 <manu34@free.fr>
2024-01-27 10:30:23 -0300

    front: configure code relead hooks

--- src/front/app.cljs
+++ src/front/app.cljs
@@ -1,4 +1,28 @@
 (ns app)
 
 (defn run []
-  (js/alert "boo"))
+  (js/console.log "boo "))
+
+;;  Lifecycle Hooks =================================
+;; see https://shadow-cljs.github.io/docs/UsersGuide.html#_lifecycle_hooks
+
+(defn ^:dev/before-load stop []
+  (js/console.log "/before-load"))
+
+(defn ^:dev/after-load start []
+  (js/console.log "after-load")
+  (run))
+
+(defn ^:dev/before-load-async async-stop [done]
+  (js/console.log "(async) stop ")
+  (js/setTimeout
+   (fn []
+     (js/console.log "(async) stop complete")
+     (done))))
+
+(defn ^:dev/after-load-async async-start [done]
+  (js/console.log "(async) start")
+  (js/setTimeout
+   (fn []
+     (js/console.log "(async) start complete")
+     (done))))
