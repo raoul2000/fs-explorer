d4b63764f0493eba2c3f5683116b61bfaaf3307d
raoul2000 <manu34@free.fr>
2024-01-27 13:50:09 -0300

    front: add scripts to start watch app and test

---
+++ tools/start-front.bash
@@ -0,0 +1,5 @@
+
+echo "starting shadow-clsj server - watch app..." 
+
+export PATH="/c/Program Files/Java/jdk-17.0.1/bin:$PATH"
+npx shadow-cljs watch app
---
+++ tools/start-test.bash
@@ -0,0 +1,5 @@
+
+echo "starting shadow-clsj server - watch test..." 
+
+export PATH="/c/Program Files/Java/jdk-17.0.1/bin:$PATH"
+npx shadow-cljs watch test
