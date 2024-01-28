a29a63c93e3a8ff6cbeac3d267e1499f26cd1ae5
raoul2000 <manu34@free.fr>
2024-01-28 10:15:24 -0300

    back: add tools.namespace

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,12 +1,40 @@
 (ns user
-  (:require [portal.api :as p]))
+  (:require [portal.api :as p]
+            [clojure.tools.namespace.repl :refer (refresh refresh-all)]
+            [integrant.core :as ig]
+            [system :refer (config)]))
+
+(comment
+
+  ;; portal ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  ;; see https://github.com/djblue/portal#api for setup
+
+  (def p (p/open {:launcher :vs-code}))
+  (add-tap #'p/submit)
+
+  (tap> "hello")
+  (p/clear)
 
 
-;; see https://github.com/djblue/portal#api for setup
+  ;; tools.namespace ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def p (p/open {:launcher :vs-code}))
-(add-tap #'p/submit)
+  ;; see https://github.com/clojure/tools.namespace/
+  ;; see https://cognitect.com/blog/2013/06/04/clojure-workflow-reloaded
 
-(tap> "hello")
-(p/clear)
+  (refresh)
+  (refresh-all)
+
+
+  ;; system ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  ;; start the system
+  (def system (ig/init config))
+
+  ;; stop the system
+  (ig/halt! system)
+
+
+  ;;
+  )
 
