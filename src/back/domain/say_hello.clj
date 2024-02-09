bd8ac863765e8cccce07de157b2d54b988cc2c4f
raoul2000 <manu34@free.fr>
2024-02-09 20:20:53 -0300

    back: more tests on error handling

--- src/back/domain/say_hello.clj
+++ src/back/domain/say_hello.clj
@@ -4,6 +4,7 @@
 (defn say-hello [name polite?]
   (if (= "bob" name)
     (throw (ex-info "user not allowed" {:name name}))
+    #_(throw (Exception. "java exception"))
     (format (if polite?
               "Good morning %s !"
               "Hi %s !") (or name "stranger"))))
--- src/dev/error_handler.clj
+++ src/dev/error_handler.clj
@@ -9,16 +9,17 @@
   (try
     (p/clear)
     
-    (/ 1 0)
+    #_(/ 1 0)
     #_(throw (ex-info "error message" {:info "extra info"}))
 
     #_(throw (new java.lang.ArithmeticException "msg"))
+    (throw (ex-info "error message" {:info "the info"}))
 
     (catch Exception ex
       (tap> {:ex-data    (ex-data ex)
              :ex-message (ex-message ex)
              :ex-cause   (ex-cause ex)})
-      (print "error")))
+      ))
 
   ;;
   )
