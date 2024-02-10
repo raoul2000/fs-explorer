289aad776b5e751e98687b045594596ec14591d5
raoul2000 <manu34@free.fr>
2024-02-10 16:23:44 -0300

    front:  start shadow-cljs script now watched both app and test

--- tools/start-front.bash
+++ tools/start-front.bash
@@ -1,5 +1,7 @@
 
-echo "starting shadow-clsj server - watch app..." 
+echo ""
+echo "starting shadow-clsj server - watch app test" 
+echo ""
 
 export PATH="/c/Program Files/Java/jdk-17.0.1/bin:$PATH"
-npx shadow-cljs watch app
+npx shadow-cljs watch app test
--- tools/start-test.bash
+++
@@ -1,5 +0,0 @@
-
-echo "starting shadow-clsj server - watch test..." 
-
-export PATH="/c/Program Files/Java/jdk-17.0.1/bin:$PATH"
-npx shadow-cljs watch test
