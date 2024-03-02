f63990db0e3f7df5783eb2edaf1d952f37516ae1
raoul2000 <manu34@free.fr>
2024-03-02 13:13:07 -0300

    front: fix navbar re-render when route changes

--- src/front/components/navbar.cljs
+++ src/front/components/navbar.cljs
@@ -6,9 +6,8 @@
 
 
 (defn navbar-item-explore
-  "The nav item 'Explore' href must always refer to the latest browsed dir and so this
+  "The nav item 'Explore' href must always refer to the current browsed dir and so this
    component is re-rendered each time the dir being explored changes."
-
   [route-name show-burger]
   (js/console.log "rendering navbar-item-explore")
   (let [current-dir (<current-dir)]
@@ -25,41 +24,42 @@
    - when local state *show-burger* changes meaning user open/close the navbar when displayed as dropdown
    - when *current-route* changes so to underline the nav item that matches the current route"
   []
-  (let [show-burger   (rc/atom false)
-        current-route (<current-route)
-        route-name    (get-in current-route [:data :name])]
-    (js/console.log "rendering navbar")
-    [:nav.navbar.is-fixed-top.has-shadow {:role "navigation", :aria-label "main navigation"}
-     [:div.navbar-brand
-      [:a.navbar-item {:href ""}
-       [:img {:src     "/image/cljs-logo.png",
-              :alt     "logo",
-              :height  "28"}]]
+  (let [show-burger   (rc/atom false)]
+    (fn []
+      (let [current-route (<current-route)
+            route-name    (get-in current-route [:data :name])]
+        (js/console.log "rendering navbar")
+        [:nav.navbar.is-fixed-top.has-shadow {:role "navigation", :aria-label "main navigation"}
+         [:div.navbar-brand
+          [:a.navbar-item {:href ""}
+           [:img {:src     "/image/cljs-logo.png",
+                  :alt     "logo",
+                  :height  "28"}]]
 
-                          ;; always visible
-      [:a.navbar-item {:title  "search (ctrl+k)"
-                       :href   ""}
-       "search"]
+           ;; always visible
+          [:a.navbar-item {:title  "search (ctrl+k)"
+                           :href   ""}
+           "search"]
 
-                          ;; burger control - displayed driven by *show-burger* r/atom
-      [:a {:role "button", :class (str "navbar-burger " (when @show-burger "is-active")), :aria-label "menu", :aria-expanded "false"
-           :on-click #(swap! show-burger not)}
-       [:span {:aria-hidden "true"}]
-       [:span {:aria-hidden "true"}]
-       [:span {:aria-hidden "true"}]]]
+          ;; burger control - displayed driven by *show-burger* r/atom
+          [:a {:role "button", :class (str "navbar-burger " (when @show-burger "is-active")), :aria-label "menu", :aria-expanded "false"
+               :on-click #(swap! show-burger not)}
+           [:span {:aria-hidden "true"}]
+           [:span {:aria-hidden "true"}]
+           [:span {:aria-hidden "true"}]]]
 
-                         ;; responsive
-     [:div {:class (str "navbar-menu " (when @show-burger "is-active"))}
-      [:div.navbar-end
+                                     ;; responsive
+         [:div {:class (str "navbar-menu " (when @show-burger "is-active"))}
+          [:div.navbar-end
 
-                           ;; Nav items must force burger control to close when they are clicked
-       [navbar-item-explore route-name show-burger]
-       [:a.navbar-item {:href ""
-                        :title "settings"}
-        "item 1"]
+        ;; Nav items must force burger control to close when they are clicked
+           [navbar-item-explore route-name show-burger]
+           [:a.navbar-item {:href ""
+                            :title "settings"}
+            "item 1"]
 
-       [:a.navbar-item {:class    (when (about-route? route-name) "is-underlined")
-                        :on-click #(reset! show-burger false)
-                        :href     (create-url-about)
-                        :title    "about"}
-        "About"]]]]))
+           [:a.navbar-item {:class    (when (about-route? route-name) "is-underlined")
+                            :on-click #(reset! show-burger false)
+                            :href     (create-url-about)
+                            :title    "about"}
+            "About"]]]]))))
