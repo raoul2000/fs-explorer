6d3e6977ae276620d5abf908975328cc946c37f9
raoul2000 <manu34@free.fr>
2024-03-11 19:41:29 -0300

    front: change folder icon color

--- src/front/components/icon.cljs
+++ src/front/components/icon.cljs
@@ -1,12 +1,12 @@
 (ns components.icon
   (:require [phosphor.icons :as icons]))
 
-(def app-icons {:folder-icon   (icons/icon :phosphor.regular/folder-simple)
+(def app-icons {:folder-icon   (icons/icon :phosphor.fill/folder-simple)
                 :file-icon     (icons/icon :phosphor.regular/file)
                 :question-icon (icons/icon :phosphor.regular/question)
                 
                 })
 
-(def folder-icon (icons/render (:folder-icon app-icons)     {:size "20px"})) 
+(def folder-icon (icons/render (:folder-icon app-icons)     {:size "20px" :color "#ffa03a"})) 
 (def file-icon   (icons/render (:file-icon app-icons)       {:size "20px"})) 
 (def about-icon  (icons/render (:question-icon app-icons)   {:size "20px"})) 
