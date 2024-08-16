fbf26475b73589dc0400b4b63e37e655ce2606a0
raoul2000 <manu34@free.fr>
2024-08-16 14:22:32 +0200

    back : more config stuff ...

--- src/dev/config.clj
+++ src/dev/config.clj
@@ -2,7 +2,9 @@
   (:require [clj-yaml.core :as yaml]
             [clojure.java.io :as io]
             [clojure.spec.alpha :as spec]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.walk :as w]
+            [clojure.data.json :as json]))
 
 ;; config is a map built by merging default config settings and, when available, user defined settings
 ;; - User defined settings overload default settings
@@ -32,24 +34,115 @@
 
 (comment
   (def config-1 #:config{:server-port 12
-                 :root-dir-path "/tmp"
-                 :open-browser true
-                 :browse-url "http://localhost"
-                 :types  {:MY_FIRST_TYPE #:config.type{:selectors {:pred  "string"}}
-                          :MY_SECOND_TYPE #:config.type{:selectors {:pred  "string"
-                                                                    :other-pred "arg2"}}}})
-  (get-in config-1 [:config/types :MY_FIRST_TYPE :config.type/selectors])
-  
+                         :root-dir-path "/tmp"
+                         :open-browser true
+                         :browse-url "http://localhost"
+                         :types  {:MY_FIRST_TYPE #:config.type{:selectors {:pred  "string"}}
+                                  :MY_SECOND_TYPE #:config.type{:selectors {:pred  "string"
+                                                                            :other-pred "arg2"}}}})
   (spec/valid? :config/map config-1)
+
+  ;; reading config values using namespaced keys : 
+
+  (:config/server-port config-1)
+  (get-in config-1 [:config/types :MY_FIRST_TYPE :config.type/selectors])
+  ;; all declared types (as keywords)
+  (keys (:config/types config-1))
+
+  ;; all declared types (as strings)
+  (map name (keys (:config/types config-1)))
+
   ;;
   )
 
 
+;; loading and prasing a YAML file into a map
+
+(def conf (yaml/parse-stream (io/reader "./test/back/fixtures/config-1.yaml")))
+
+;; ... but the map returned does not include any namespace : we must add them
+;; Let's create function for this 
+
+(defn add-ns-to-key [ns-name k]
+  (keyword ns-name (name k)))
+
+(defn add-ns-to-map
+  "Add *ns-name* namespace to all keys of map *m*.
+   When a key is a string it is converted into a keyword.
+   When a key is a keyword already with a namespace, it is replaced by the given ns"
+  [ns-name m]
+  (into {} (map (fn [[k v]]
+                  [(add-ns-to-key ns-name k) v]) m)))
+
+(defn build-types [m]
+  (into {} (map (fn [[k v]]
+                  [k (add-ns-to-map "config.type" v)]) m)))
+
+(defn add-ns-to-config [m]
+  (w/walk (fn [[k v]]
+            (let [ns-key (add-ns-to-key "config" k)]
+              [ns-key (case ns-key
+                        :config/types     (build-types   v)
+                        v)])) identity m))
 
 (comment
-  ;; loading and prasing a YAML file into a map
 
-  (def conf (yaml/parse-stream (io/reader "./test/back/fixtures/file-1.yaml")))
+  (spec/valid?  :config/map (add-ns-to-config conf))
+  (spec/explain :config/map (add-ns-to-config conf))
+
+  ;; now turn the config map into json
+  ;; does not preserve namespace
+  (print (json/write-str conf :escape-slash false))
+
   ;;
   )
+
+(comment
+  ;; Let's review the merging process between default config and user defined config
+
+  (def default-config #:config{:server-port 8880
+                               :root-dir-path "/tmp"
+                               :open-browser true
+                               :browse-url "http://localhost"
+                               :types  {:file #:config.type{:selectors {:is-file  "string"}}
+                                        :folder #:config.type{:selectors {:is-folder  "string"}}}})
+
+  ;; of course, default config should be valid
+  (spec/valid? :config/map default-config)
+
+  ;; see https://clojure.github.io/clojure-contrib/map-utils-api.html#clojure.contrib.map-utils/deep-merge-with
+  (defn deep-merge-with
+    "Like merge-with, but merges maps recursively, applying the given fn
+  only when there's a non-map at a particular level.
+
+  (deepmerge + {:a {:b {:c 1 :d {:x 1 :y 2}} :e 3} :f 4}
+               {:a {:b {:c 2 :d {:z 9} :z 3} :e 100}})
+  -> {:a {:b {:z 3, :c 3, :d {:z 9, :x 1, :y 2}}, :e 103}, :f 4}"
+    [f & maps]
+    (apply
+     (fn m [& maps]
+       (if (every? map? maps)
+         (apply merge-with m maps)
+         (apply f maps)))
+     maps))
+
+  ;; let's create a partial user-config : it is not conform to spec because partial
+  (def config-2 #:config{:root-dir-path "/my/default/folder"
+                         :open-browser true
+                         :browse-url "http://localhost"
+                         :types  {:MY_FIRST_TYPE #:config.type{:selectors {:pred  "string"}}}})
+
+  (def user-config (add-ns-to-config config-2))
+  (def final-config (deep-merge-with  (fn [a b] b) default-config  user-config))
+
+  (spec/valid? :config/map final-config)
+
+  (= 8880 (:config/server-port final-config))
+  (= "/tmp" (:config/root-dir-path final-config))
+
+  ;;
+  )
+
+
+
 
